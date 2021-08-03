e42981c396e24ece9105e0b150ec530079fc7aed
hakanolgun <kaptanhakanolgun@gmail.com>
2021-08-03 12:01:09 +0300

    user login

--- client/src/pages/Auth/Signup/index.js
+++ client/src/pages/Auth/Signup/index.js
@@ -1,11 +1,91 @@
-import React from 'react'
+import React from "react";
+import {
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+} from "@chakra-ui/react";
+import { useFormik } from "formik";
+import validationSchema from "./validations";
+
+import { fetchRegister } from "../../../api";
 
 function Signup() {
-    return (
-        <div>
-            Signup
-        </div>
-    )
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+    validationSchema,
+    onSubmit: async (values, bag) => {
+      try {
+        const registerResponse = await fetchRegister({
+          email: values.email,
+          password: values.password,
+        });
+        console.log(registerResponse);
+      } catch (e) {}
+    },
+  });
+
+  return (
+    <div>
+      <Flex align="center" width="full" justifyContent="center">
+        <Box pt={10}>
+          <Box textAlign="center">
+            <Heading>Sign Up</Heading>
+          </Box>
+          <Box my={5} textAlign="left">
+            <form onSubmit={formik.handleSubmit}>
+              <FormControl>
+                <FormLabel>E-Mail</FormLabel>
+                <Input
+                  name="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                  isInvalid={formik.touched.email && formik.errors.email}
+                />
+              </FormControl>
+              <FormControl mt="4">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.password}
+                  isInvalid={formik.touched.password && formik.errors.password}
+                />
+              </FormControl>
+              <FormControl mt="4">
+                <FormLabel>Password Confirm</FormLabel>
+                <Input
+                  name="passwordConfirm"
+                  type="password"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.passwordConfirm}
+                  isInvalid={
+                    formik.touched.passwordConfirm &&
+                    formik.errors.passwordConfirm
+                  }
+                />
+              </FormControl>
+
+              <Button mt="4" width="full" type="submit">
+                Sign Up
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
